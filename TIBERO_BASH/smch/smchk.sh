4fa20b400e5e4115d0cac07280ae59a006d72f98
younjunsu <junsu_youn@naver.com>
2021-12-29 13:55:01 +0900

    Update smchk.sh

--- TIBERO_BASH/smch/smchk.sh
+++ TIBERO_BASH/smch/smchk.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/ksh
+#!/usr/bin/bash
 #-------------------------------------------------------------------------------
 # @File name      : smchk.sh
 # @Contents       : 
@@ -10,11 +10,24 @@
 # xxxx.xx.xx xxxxx                 (Verxx)
 # ------------------------------------------------------------------------------
 
+### RUN CHECK
+
 
 ### Summary
-## Sysmaster Process Update
+function fn_sysmaster_summary(){
+    echo "UP-Time"
+    uptime -s |awk '{print "Server Time : "$1" "$2}'
 
+    echo "TIBERO UP-Time"
+    ps -eo lstart,pid,cmd |grep $DB_LIST_02|grep -vE "grep|cub_admin"| awk '{
+    cmd="date -d\""$1 FS $2 FS $3 FS $4 FS $5"\" +\047%Y-%m-%d %H:%M:%S\047"; 
+    cmd | getline d; close(cmd); $1=$2=$3=$4=$5=""; printf "%s\n",d$0 }' 2>/dev/null|awk '{print $1" "$2}'
 
+    echo "JEUS UP-Time"
+
+    
+    echo "Hyper Loader UP-Time"
+}
 
 ### JEUS Detail
 
@@ -25,7 +38,14 @@
 ### Repository TIBERO Detail
 tbsql sys/tibero @smchk.sql <<EOF
 quit
+
+
 EOF
 
 ### Control TIBERO Detail
 
+
+
+
+
+  
