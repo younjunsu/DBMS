7f597cec77cd1074388b914b74ba01612214c946
younjunsu <junsu_youn@naver.com>
2021-12-30 10:03:50 +0900

    Update smchk.sh

--- TIBERO_BASH/smch/smchk.sh
+++ TIBERO_BASH/smch/smchk.sh
@@ -10,32 +10,35 @@
 # xxxx.xx.xx xxxxx                 (Verxx)
 # ------------------------------------------------------------------------------
 
-### RUN CHECK
 
-
-### Summary
-function fn_sysmaster_summary(){
-    echo "UP-Time"
-    uptime -s |awk '{print "Server Time : "$1" "$2}'
-
-    echo "TIBERO UP-Time"
-    ps -eo lstart,pid,cmd |grep $DB_LIST_02|grep -vE "grep|cub_admin"| awk '{
+    echo "######## 1. Started Time ########"
+# SYSTEM
+    echo "######## 1.1 System"
+    echo "System -> "`uptime -s |awk '{print $1" "$2}'`
+    echo
+# SMDB
+    SMDB_PID=(`ps -ef |grep -w tbsvr |grep -v grep |awk '{print $2}'`)
+    SMDB_STARTTIME=`ps -eo lstart,pid,cmd |grep $SMDB_PID|grep -vE "grep|cub_admin"| awk '{
     cmd="date -d\""$1 FS $2 FS $3 FS $4 FS $5"\" +\047%Y-%m-%d %H:%M:%S\047"; 
-    cmd | getline d; close(cmd); $1=$2=$3=$4=$5=""; printf "%s\n",d$0 }' 2>/dev/null|awk '{print $1" "$2}'
+    cmd | getline d; close(cmd); $1=$2=$3=$4=$5=""; printf "%s\n",d$0 }' 2>/dev/null |awk '{print $1" "$2}'`
+    echo "######## 1.2 SMDB"
+    echo "SMDB -> PID : "$SMDB_PID", Start-Time : "$SMDB_STARTTIME
+    echo
+#JEUS
+    echo "######## 1.3 JEUS"
+    jps |grep -v Jps |awk '{print "PID : "$1", CMD : "$2}'
 
-    echo "JEUS UP-Time"
-
+#Hyper Loader
+    echo "######## 1.4 HyperLoader"
     
-    echo "Hyper Loader UP-Time"
-}
-
-### JEUS Detail
-
-
-### Hyperloader Detail
+## Process and listening port check
+    echo ""######## 2. Port check ########"
+    echo 
+    echo "Proto Recv-Q Send-Q Local Address           Foreign Address         State       PID/Program name    "
+    netstat -nlp |grep tcp 
 
 
-### Repository TIBERO Detail
+
 tbsql sys/tibero @smchk.sql <<EOF
 quit
 
